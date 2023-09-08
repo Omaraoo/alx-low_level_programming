1ba7eb17a2b5119ce727ed3662cc4385cceed9aa
Omaraoo <omaraoo98@gmail.com>
2023-09-08 02:55:36 +0000

    44

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description" A C program with puts
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
