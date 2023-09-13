404c350b674fadf0fbfb5a499450516eb5e9205f
Omaraoo <omaraoo98@gmail.com>
2023-09-13 02:26:30 +0000

    v3

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - prints the alphabets
+ *
+ * print_alphabet - print all alphanet lower case
+ *
+ * Return: 0
+*/
+
+void print_alphabet(void)
+{
+char c;
+for (c = 97; c <= 122; c++)
+putchar (c);
+putchar ('\n');
+}
