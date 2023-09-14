58ac09e89a71f1567a77c37e47858391e0ff18b9
Omaraoo <omaraoo98@gmail.com>
2023-09-14 23:30:36 +0000

    6

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -12,9 +12,8 @@
  * Return: Always 0 (success)
 */
 
-int main(void)
+void positive_or_negative(int n)
 {
-int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
@@ -23,5 +22,4 @@ if (n == 0)
 	printf("%u is zero\n", n);
 if (n < 0)
 	printf("%d is negative\n", n % 100);
-return (0);
 }
