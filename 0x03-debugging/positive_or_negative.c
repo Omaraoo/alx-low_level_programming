971aad531bb4182fe5557c0a67196481f7be567d
Omaraoo <omaraoo98@gmail.com>
2023-09-14 23:16:33 +0000

    4

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: false or true validation
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+	printf("%u is positive\n", n);
+if (n == 0)
+	printf("%u is zero\n", n);
+if (n < 0)
+	printf("%d is negative\n", n % 100);
+return (0);
+}
