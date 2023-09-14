659449d713a155734b41b30f64f1536e1f2f0b9c
Omaraoo <omaraoo98@gmail.com>
2023-09-14 23:35:14 +0000

    7

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -5,21 +5,19 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Description: false or true validation
- *
+ * positive_or_negative - check if the int is positive or negative
+ * @i: the int to be randomize
  * Return: Always 0 (success)
 */
 
-void positive_or_negative(int n)
+void positive_or_negative(int i)
 {
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n > 0)
-	printf("%u is positive\n", n);
-if (n == 0)
-	printf("%u is zero\n", n);
-if (n < 0)
-	printf("%d is negative\n", n % 100);
+if (i > 0)
+	printf("%u is positive\n", i);
+if (i == 0)
+	printf("%u is zero\n", i);
+if (i < 0)
+	printf("%d is negative\n", i % 100);
 }
