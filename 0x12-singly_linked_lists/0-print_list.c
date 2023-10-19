5dc13b7f3cc57606fad453681730605a477d11be
Omaraoo <omaraoo98@gmail.com>
2023-10-19 09:56:15 -0700

    33 git commit -m 33
    git commit -m 33

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,7 +6,8 @@
  * @h: pointer to the list_t list to print
  *
  * Return: the number of nodes printed
- */
+*/
+
 size_t print_list(const list_t *h)
 {
 size_t s = 0;
@@ -20,6 +21,5 @@ printf("[%u] %s\n", h->len, h->str);
 h = h->next;
 s++;
 }
-
 return (s);
 }
